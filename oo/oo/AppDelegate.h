b1c1a2e41624f35cafcf5a4fc41841d25e74b8b2
humengchen <1533592021@qq.com>
2016-03-01 09:49:01 +0800

    dd

---
+++ oo/oo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oo
+//
+//  Created by ljky on 16/3/1.
+//  Copyright © 2016年 蓝科实验室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oo/oo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  oo
+//
+//  Created by ljky on 16/3/1.
+//  Copyright © 2016年 蓝科实验室. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
